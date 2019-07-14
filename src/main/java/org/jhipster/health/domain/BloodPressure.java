803127be69984416f9ae7e5340bf26c1ae835d9f
Thierry Herve <thierry_herve@yahoo.fr>
2019-07-14 17:23:05 +0200

    Ajout de l'entité BloodPressure
    Ajout de l'entité BloodPressure

---
+++ src/main/java/org/jhipster/health/domain/BloodPressure.java
@@ -0,0 +1,131 @@
+package org.jhipster.health.domain;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+
+import org.springframework.data.elasticsearch.annotations.FieldType;
+import java.io.Serializable;
+import java.time.LocalDate;
+
+/**
+ * A BloodPressure.
+ */
+@Entity
+@Table(name = "blood_pressure")
+@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
+@org.springframework.data.elasticsearch.annotations.Document(indexName = "bloodpressure")
+public class BloodPressure implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
+    @SequenceGenerator(name = "sequenceGenerator")
+    @org.springframework.data.elasticsearch.annotations.Field(type = FieldType.Keyword)
+    private Long id;
+
+    @NotNull
+    @Column(name = "date", nullable = false)
+    private LocalDate date;
+
+    @Column(name = "systolic")
+    private Integer systolic;
+
+    @Column(name = "diastolic")
+    private Integer diastolic;
+
+    @ManyToOne
+    @JsonIgnoreProperties("bloodPressures")
+    private User user;
+
+    // jhipster-needle-entity-add-field - JHipster will add fields here, do not remove
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public BloodPressure date(LocalDate date) {
+        this.date = date;
+        return this;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Integer getSystolic() {
+        return systolic;
+    }
+
+    public BloodPressure systolic(Integer systolic) {
+        this.systolic = systolic;
+        return this;
+    }
+
+    public void setSystolic(Integer systolic) {
+        this.systolic = systolic;
+    }
+
+    public Integer getDiastolic() {
+        return diastolic;
+    }
+
+    public BloodPressure diastolic(Integer diastolic) {
+        this.diastolic = diastolic;
+        return this;
+    }
+
+    public void setDiastolic(Integer diastolic) {
+        this.diastolic = diastolic;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public BloodPressure user(User user) {
+        this.user = user;
+        return this;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+    // jhipster-needle-entity-add-getters-setters - JHipster will add getters and setters here, do not remove
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof BloodPressure)) {
+            return false;
+        }
+        return id != null && id.equals(((BloodPressure) o).id);
+    }
+
+    @Override
+    public int hashCode() {
+        return 31;
+    }
+
+    @Override
+    public String toString() {
+        return "BloodPressure{" +
+            "id=" + getId() +
+            ", date='" + getDate() + "'" +
+            ", systolic=" + getSystolic() +
+            ", diastolic=" + getDiastolic() +
+            "}";
+    }
+}
